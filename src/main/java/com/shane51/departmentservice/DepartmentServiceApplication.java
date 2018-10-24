1b5db34293c1f925071a519089f385264b4a51b5
Wuyi Qin <wyqin@thoughtworks.com>
2018-10-24 17:34:11 +0800

    [Tech] Enable swagger2

--- src/main/java/com/shane51/departmentservice/DepartmentServiceApplication.java
+++ src/main/java/com/shane51/departmentservice/DepartmentServiceApplication.java
@@ -7,14 +7,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableSwagger2
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DepartmentServiceApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shane51.departmentservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee V1.0").build());
     }
 
 
